5eda2b08cb1a73148572711d6d3d4b0aa4a40448
Fridayk7 <u04pk18@abdn.ac.uk>
2019-12-11 04:36:07 +0000

    Price

--- db/migrate/20191211005021_change_price_to_be_float_in_stocks.rb
+++ db/migrate/20191211005021_change_price_to_be_float_in_stocks.rb
@@ -1,5 +1,5 @@
 class ChangePriceToBeFloatInStocks < ActiveRecord::Migration[5.2]
   def change
-     change_column :stocks, :price, :float
+     change_column :stocks, :price, :decimal, :precision => 8, :scale => 2
   end
 end
